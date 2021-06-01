d2c65d430cadaa520d1ebaf7f164e14c27917873
Sean Aird <seanraird@yahoo.com>
2021-06-01 18:07:12 -0400

    commit tree 5696a41253f2d06ce5d7b44f665c174d33e9c502

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solution in this file!
+const Hq = 42;
+const Pickup = 50;
+function distanceFromHqInBlocks(Blocks){
+
+
+   Blocks = Pickup - Hq;
+    return Blocks;
+    
+
+}
+
+
+function distanceFromHqInFeet(Feet){
+    Feet = 264 * 8;
+    return Feet;
+}
+
+function distanceTravelledInFeet(North, South, Distance){
+  if (North){
+        Distance = (North - Hq)*264;
+    return Distance;} else if (South){
+
+    Distance = (Hq - South)*264;
+    return Distance;
+    }
+
+}
